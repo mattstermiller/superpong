86beb64d19d7cbfedc0508252f1cb827c2485a12
Matt Miller <miller.mattster@gmail.com>
2019-12-07 13:18:35 -0600

    Adds basic F# MonoGame app.
    Based on https://github.com/ChrisPritchard/FSharpMonogameTemplate

---
+++ SuperPong/Program.fs
@@ -0,0 +1,27 @@
+﻿namespace SuperPong
+
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics;
+open Microsoft.Xna.Framework.Input;
+
+type PongGame() as this = 
+    inherit Game()
+
+    let graphics = new GraphicsDeviceManager(this)
+    let mutable spriteBatch = Unchecked.defaultof<SpriteBatch>
+
+    override __.LoadContent() = 
+        spriteBatch <- new SpriteBatch(this.GraphicsDevice)
+
+    override __.Update(gameTime) =
+        ()
+
+    override __.Draw(gameTime) =
+        this.GraphicsDevice.Clear Color.LemonChiffon
+
+module Entry =
+    [<EntryPoint>]
+    let main _ =
+        use game = new PongGame()
+        game.Run()
+        0
